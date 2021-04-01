8457d1bf349937bc579a82c02df8546c944489c1
ArcanumLibella <fiona.roux@hetic.net>
2021-04-01 16:35:10 +0200

    feat: update state with videos data

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import Header from './components/Header'
 import youtube from './api/youtube'
 
 class App extends React.Component {
-  onTermSubmit = term => {
-    youtube.get('/search', {
+  state = { videos: [] }
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
     })
+
+    console.log(response.data.items)
+    this.setState({ videos: response.data.items })
+    console.log('my state :', this.state.videos)
   }
 
   render () {
@@ -19,6 +25,7 @@ class App extends React.Component {
         <Header />
         <main className='container max-screen-xl mx-auto'>
           <SearchBar onFormSubmit={this.onTermSubmit}/>
+          <h2>I have {this.state.videos.length} video(s) !</h2>
         </main>
       </div>
     )
