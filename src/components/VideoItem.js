e8c8f9bc74af8a4c6b0d8e18d5a3a858baded934
ArcanumLibella <fiona.roux@hetic.net>
2021-05-25 16:33:48 +0200

    fix: style videoItem on mobile

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function VideoItem({video}) {
     return (
-        <div className='videoItem flex text-left mb-2 p-4 bg-blue-200 rounded cursor-pointer hover:bg-blue-300 transition-all'>
-            <img src={video.snippet.thumbnails.medium.url} alt='Video preview' className='max-w-160 rounded' />
-            <div className='ml-2'>
+        <div className='videoItem flex flex-col md:flex-row text-left mb-2 p-4 bg-blue-200 rounded cursor-pointer hover:bg-blue-300 transition-all'>
+            <img src={video.snippet.thumbnails.medium.url} alt='Video preview' className='md:max-w-160 rounded' />
+            <div className='mt-4 md:ml-2 md:mt-0'>
                 <h4 className='font-semibold'>{video.snippet.title}</h4>
                 <h5 className=''>{video.snippet.channelTitle}</h5>
             </div>
