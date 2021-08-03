15572802c11eeedcca8d63230e60bfa715fcf602
ArcanumLibella <fiona.roux@hetic.net>
2021-08-03 18:06:58 +0200

    feat: change SearchBar Class Component into Functionnal Component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-export default class SearchBar extends Component {
-  state = { term: ''}
+export default function SearchBar({ onFormSubmit }) {
+  const [term, setTerm] = useState('')
 
-  onFormSubmit = (e) => {
-    e.preventDefault();
-    this.props.onFormSubmit(this.state.term)
+  const onSubmit = (e) => {
+    e.preventDefault()
+    onFormSubmit(term)
   }
 
-  render () {
-    return (
-      <div className='searchBar p-8 m-4 mt-8 rounded-xl bg-blue-100'>
-        <form className='flex flex-col' onSubmit={this.onFormSubmit}>
-          <label className='text-left font-medium mb-2'>Looking for a video :</label>
-          <input
-            className='p-4 rounded'
-            type='text'
-            placeholder='Maxime Gasteuil'
-            value={ this.state.term }
-            onChange={ (e) => this.setState({term: e.target.value}) }
-          />
-        </form>
-      </div>
-    )
-  }
+  return (
+    <div className='searchBar p-8 m-4 mt-8 rounded-xl bg-blue-100'>
+      <form className='flex flex-col' onSubmit={onSubmit}>
+        <label className='text-left font-medium mb-2'>Looking for a video :</label>
+        <input
+          className='p-4 rounded'
+          type='text'
+          placeholder='Anax imperator'
+          value={ term }
+          onChange={ (e) => setTerm(e.target.value) }
+        />
+      </form>
+    </div>
+  )
 }
+
