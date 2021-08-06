63c9cfc7b2bf3bdd63c73864f12881700a7e7669
ArcanumLibella <fiona.roux@hetic.net>
2021-08-06 15:07:14 +0200

    feat: create a custom hook useVideos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
-import youtube from './api/youtube'
+import useVideos from './hooks/useVideos'
 
 import SearchBar from './components/SearchBar'
 import Header from './components/Header'
@@ -8,30 +8,18 @@ import VideosList from './components/VideosList'
 import VideoDetails from './components/VideoDetails'
 
 export default function App() {
-  const [videos, setVideos] = useState([])
+  const [videos, search] = useVideos('bees')
   const [selectedVideo, setSelectedVideo] = useState(null)
   
-  const onTermSubmit = async (term) => {
-    const response = await youtube.get('/search', {
-      params: {
-        q: term
-      } 
-    })
-
-    setVideos(response.data.items)
-    setSelectedVideo(response.data.items[0])
-    console.log(`videos : `, videos)
-  }
-
   useEffect(() => {
-    onTermSubmit('Anax imperator')
-  }, [])
+    setSelectedVideo(videos[0])
+  }, [videos])
 
   return (
     <div className='App'>
       <Header />
       <main className='container max-screen-xl mx-auto'>
-        <SearchBar onFormSubmit={onTermSubmit}/>
+        <SearchBar onFormSubmit={search}/>
 
         <div className='lg:flex lg:justify-between m-4 mt-8'>
           <VideoDetails video={selectedVideo} />
